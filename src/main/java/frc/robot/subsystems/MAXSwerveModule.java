deca895358a5c573d4876431fe191437a9cb02e9
kylephant <kyleeagleson4@gmail.com>
2024-02-05 05:56:27 -0600

    Remove unused imports
    Removed unused imports from MAXSwerveModule.java

--- src/main/java/frc/robot/subsystems/MAXSwerveModule.java
+++ src/main/java/frc/robot/subsystems/MAXSwerveModule.java
@@ -8,17 +8,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.SparkAbsoluteEncoder.Type;
-import com.revrobotics.SparkPIDController;
-
 import org.littletonrobotics.junction.Logger;
-
-import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.RelativeEncoder;
-
-import frc.robot.Constants.ModuleConstants;
 
 public class MAXSwerveModule {
 
