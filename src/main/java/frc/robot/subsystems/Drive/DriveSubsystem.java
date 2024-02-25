82b63f5b7f0ca9b958328398c237cbb32cf4a394
kylephant <57305097+kylephant@users.noreply.github.com>
2024-02-25 14:58:01 -0600

    Organize Drive Subsystem into Drive
    Create a folder "Drive" in "subsystems" for drive related classes

--- src/main/java/frc/robot/subsystems/Drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Drive/DriveSubsystem.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.Drive;
 
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -27,8 +27,9 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.AutoConstants;
-import edu.wpi.first.wpilibj.I2C.Port;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.GyroIO;
+import frc.robot.subsystems.GyroIOInputsAutoLogged;
 import frc.utils.SwerveUtils;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
--- src/main/java/frc/robot/subsystems/Drive/MAXModuleIO.java
+++ src/main/java/frc/robot/subsystems/Drive/MAXModuleIO.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems;
+package frc.robot.subsystems.Drive;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
