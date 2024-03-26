de212123b7c9b1fed8ed90f3f8b26e1fe27f6ef0
AdrienChew246 <s1736607@online.houstonisd.org>
2024-03-26 00:24:30 -0500

    Lowered swerve turn PID value
    - Swerve turn PID value from 3 --> 0.5

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -115,16 +115,17 @@ public final class Constants {
     public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
     public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
 
-    public static final double kDrivingP = 0.06;
+    public static final double kDrivingP = 0.04;
     public static final double kDrivingI = 0;
     public static final double kDrivingD = 0;
     public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
     public static final double kDrivingMinOutput = -1;
     public static final double kDrivingMaxOutput = 1;
 
-    public static final double kTurningP = 3; //1
+    public static final double kTurningP = 0.5; //1
     public static final double kTurningI = 0;
     public static final double kTurningD = 0;
+    
     public static final double kTurningFF = 0;
     public static final double kTurningMinOutput = -1;
     public static final double kTurningMaxOutput = 1;
