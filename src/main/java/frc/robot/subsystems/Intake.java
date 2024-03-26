9d3006e69405a4fc64f89361ce73b61f08f3f3be
kylephant <57305097+kylephant@users.noreply.github.com>
2024-03-25 20:43:50 -0500

    Make indexer work with Victor
    Changing Intake to work with Victor with one change to line of code (line 20 of Intake.java)

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,8 +4,6 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
 
 // import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,28 +11,12 @@ import frc.robot.Constants.IntakeConstants;
 
 
 public class Intake extends SubsystemBase {
-    private final CANSparkMax IntakeNEO;
+    private final IntakeIO io;
     private IntakeConstants.IntakeState intakeState;
     // private double speed; 
     
     public Intake(int intakeID) {
-        IntakeNEO = new CANSparkMax(intakeID, MotorType.kBrushless); //Brushed
-
-        // Factory reset, so we get the SPARKS MAX to a known state before configuring
-        // them. This is useful in case a SPARK MAX is swapped out.
-        IntakeNEO.restoreFactoryDefaults();
-
-        // Apply position and velocity conversion factors for the turning encoder. We
-        // want these in radians and radians per second to use with WPILib's swerve
-        // APIs.
-
-        IntakeNEO.setIdleMode(IntakeConstants.kIntakeMotorIdleMode);
-        IntakeNEO.setSmartCurrentLimit(IntakeConstants.kIntakeMotorCurrentLimit);
-
-        // Save the SPARK MAX configurations. If a SPARK MAX browns out during
-        // operation, it will maintain the above configurations.
-        IntakeNEO.burnFlash();
-
+        io = new SparkIntakeIO(intakeID);
         intakeState = IntakeConstants.IntakeState.STOPPED;
         // speed = 0;
 
@@ -47,12 +29,12 @@ public class Intake extends SubsystemBase {
 
     public void setSpeed(double speed)
     {
-        IntakeNEO.set(speed);
+        io.setSpeed(speed);
         // this.speed = speed;
     }
 
     public void setVoltage(double volts){
-        IntakeNEO.setVoltage(volts);
+        io.setVoltage(volts);
     }
 
     public void intake()
@@ -69,7 +51,7 @@ public class Intake extends SubsystemBase {
 
     public void stop()
     {
-        IntakeNEO.stopMotor();
+        io.stopMotor();
         intakeState = IntakeConstants.IntakeState.STOPPED;
     }
 
